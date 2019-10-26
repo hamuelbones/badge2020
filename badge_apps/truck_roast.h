adc12a84fecea863c6007605f38d591200006cf3
pacna <paulc1231@gmail.com>
2019-10-26 18:58:36 -0400

    Roast words for truck sim

---
+++ badge_apps/truck_roast.h
@@ -0,0 +1,3 @@
+#define FIRST_ROAST "Go back to driving school"
+#define SECOND_ROAST "This sim is an actually representation on how you drive in the real world"
+#define THIRD_ROAST "My grandma can drive better than you"
