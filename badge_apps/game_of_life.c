f9240b01928e336b4781c024482232bd267be62b
Dustin Firebaugh <dafirebaugh@gmail.com>
2020-02-22 16:28:13 -0500

    - adding grid struct - renaming my app references to "gameoflife" - randomly seeding grid based on timestamp

--- badge_apps/game_of_life.c
+++ badge_apps/game_of_life.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/time.h> /* for gettimeofday */
 #include <string.h> /* for memset */
+#include <math.h>
 #include "game_of_life.h"
 
 #include "../linux/linuxcompat.h"
@@ -18,31 +19,83 @@ extern char *strncpy(char *dest, const char *src, size_t n);
 extern void *memset(void *s, int c, size_t n);
 extern char *strcat(char *dest, const char *src);
 
+extern int timestamp;
+
 #endif
 
 #include "build_bug_on.h"
+#include "./xorshift.h"
 
-/* Program states.  Initial state is MYPROGRAM_INIT */
-enum myprogram_state_t {
-	MYPROGRAM_INIT,
-	MYPROGRAM_RUN,
-	MYPROGRAM_EXIT,
+/* GRID_SIZE must be a square number */
+#define GRID_SIZE 256
+#define CELL_SIZE 2
+#define ROW_SIZE(x) ((int)(sqrt((x))))
+
+static struct Grid {
+	struct Cell {
+		unsigned char alive;
+		unsigned int neighbor_count;
+		struct coordinates {
+			unsigned int x;
+			unsigned int y;
+		} cell_coordinates;
+	} grid_cells[GRID_SIZE];
+} game_grid;
+
+/* Program states.  Initial state is GAMEOFLIFE_INIT */
+enum gameoflife_state_t {
+	GAMEOFLIFE_INIT,
+	GAMEOFLIFE_RUN,
+	GAMEOFLIFE_EXIT,
 };
 
-static enum myprogram_state_t myprogram_state = MYPROGRAM_INIT;
+static enum gameoflife_state_t gameoflife_state = GAMEOFLIFE_INIT;
+
+static void render_grid(){
+	int n;
+	for (n = 0; n < GRID_SIZE; n++){
+		#ifdef __linux__
+			// printf("random 1 or 0: %d\n", xorshift((unsigned int *)&timestamp) % 2);
+			printf("%d", game_grid.grid_cells[n].alive);
+
+			/* split into rows */
+			if((n + 1) % ROW_SIZE(GRID_SIZE) == 0){
+				printf("\n");
+			}
+			
+		#endif
+	}
+
+	#ifdef __linux__
+		printf("\n"); /* just separating with a newline for now*/
+	#endif
+}
+
+static void init_grid(){
+	int n;
+
+	for (n = 0; n < GRID_SIZE; n++){
+		game_grid.grid_cells[n].alive = xorshift((unsigned int *)&timestamp) % 2;
+	}
+}
 
 static void myprogram_init(void)
 {
+	#ifdef __linux__
+		printf("grid size %d\nrow size: %d\n\n", GRID_SIZE, ROW_SIZE(GRID_SIZE));
+	#endif
 	FbInit();
 	FbClear();
-	myprogram_state = MYPROGRAM_RUN;
+	gameoflife_state = GAMEOFLIFE_RUN;
 }
+
 
 static void check_buttons()
 {
+
 	if (BUTTON_PRESSED_AND_CONSUME) {
-		/* Pressing the button exits the program. You probably want to change this. */
-		myprogram_state = MYPROGRAM_EXIT;
+		init_grid();
+		render_grid();
 	} else if (LEFT_BTN_AND_CONSUME) {
 	} else if (RIGHT_BTN_AND_CONSUME) {
 	} else if (UP_BTN_AND_CONSUME) {
@@ -66,21 +119,21 @@ static void myprogram_run()
 
 static void myprogram_exit()
 {
-	myprogram_state = MYPROGRAM_INIT; /* So that when we start again, we do not immediately exit */
+	gameoflife_state = GAMEOFLIFE_INIT; /* So that when we start again, we do not immediately exit */
 	returnToMenus();
 }
 
 /* You will need to rename myprogram_cb() something else. */
 int game_of_life_cb(void)
 {
-	switch (myprogram_state) {
-	case MYPROGRAM_INIT:
+	switch (gameoflife_state) {
+	case GAMEOFLIFE_INIT:
 		myprogram_init();
 		break;
-	case MYPROGRAM_RUN:
+	case GAMEOFLIFE_RUN:
 		myprogram_run();
 		break;
-	case MYPROGRAM_EXIT:
+	case GAMEOFLIFE_EXIT:
 		myprogram_exit();
 		break;
 	default:
